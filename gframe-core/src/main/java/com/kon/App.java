454a5892e6fbeef55a19ca5bef286b63962a03b1
GongWei <blackjackhoho@gmail.com>
2014-06-05 00:45:56 +0800

    first commit
    first commit

---
+++ gframe-core/src/main/java/com/kon/App.java
@@ -0,0 +1,13 @@
+package com.kon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gframe-core/src/main/java/com/kon/MyTestMain.java
@@ -0,0 +1,10 @@
+package com.kon;
+
+/**
+ * Created by konbluesky on 14-6-4.
+ */
+public class MyTestMain {
+    public void SayHello(){
+        System.out.println("Hello world!!!! gongwei");
+    }
+}
